3c93a92f29df81dc761a7e28c552d2e297162a5a
Nicolas <Nicolas@40pieceKFC>
2015-10-05 16:35:20 -0600

    comments

--- src/selectivesender/Sender.java
+++ src/selectivesender/Sender.java
@@ -19,23 +19,25 @@ import java.util.TimerTask;
  * @author Nicolas Moore
  *
  */
+
 public class Sender {
 
-    Scanner in = new Scanner(System.in);
-    public static final int size = 128;
-    int window = 0;
-    int sequence = Integer.MAX_VALUE;
-    int drop[] = new int[size];
+    Scanner in = new Scanner(System.in);        // Initialize scanner
+    public static final int size = 128;         // Set size variable for data
+    int window = 0;                             // Set window base
+    int sequence = Integer.MAX_VALUE;           // Initialize sequence number
+    int drop[] = new int[size];                 // Initialize array for dropped packets
     
+    // Class for setting up timers on packets
     class Resend extends TimerTask{
         
-        int packetNumber;
-        DatagramSocket resendSocket;
-        DatagramPacket resendPkt;
-        InetAddress IPAddress;
-        byte resendData[] = new byte[size];
+        int packetNumber;                       // Packet number reference
+        DatagramSocket resendSocket;            // Socket to resend on
+        DatagramPacket resendPkt;               // Packet to resend
+        InetAddress IPAddress;                  // Destination address
+        byte resendData[] = new byte[size];     // Data array to resend
         
-        
+        // Constructor for resend class, sets appropriate parameters
         public  Resend(int i, DatagramSocket socket, DatagramPacket pkt){
             packetNumber = i;
             resendSocket = socket;
@@ -43,53 +45,54 @@ public class Sender {
             IPAddress = pkt.getAddress();
         }
         
+        // Override run method in TimerTask class to resend timed out packet
         @Override
         public void run() {
-            System.out.println("Packet "+ packetNumber+ " times out. resend packet "+packetNumber);
-            resendData[0] = (byte) packetNumber;
-            resendPkt = new DatagramPacket(resendData, resendData.length, IPAddress, 9876);
+            System.out.println("Packet "+ packetNumber+ " times out. resend packet "+packetNumber);     // Print packet being resent
+            resendData[0] = (byte) packetNumber;                                                        // Set the sequence number 
+            resendPkt = new DatagramPacket(resendData, resendData.length, IPAddress, 9876);             // Create packet to resend
             try {
-                resendSocket.send(resendPkt);
+                resendSocket.send(resendPkt);                                                           // Resend packet
             } catch (IOException ex) {
-                Logger.getLogger(Sender.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(Sender.class.getName()).log(Level.SEVERE, null, ex);                   // Catch IOExceptions
             }
-            System.out.println("Packet "+packetNumber+" is resent.");
+            System.out.println("Packet "+packetNumber+" is resent.");                                   // Print packet is resent
         }
         
-        
-        
     }
-
+    
+    // Main method
     public static void main(String ARGS[]) {
         Sender start = new Sender();
     }
-
+    
+    // Constructor
     public Sender() {
-        setWindow();
-        setSequence();
-        if (window * 2 > sequence) {
+        setWindow();        // Get window size
+        setSequence();      // Get number of segments
+        if (window * 2 > sequence) {    // If window size is to large ask for new one
             do {
                 System.out.println("Your window cannot be over half the size of your max sequence number.\n");
                 setWindow();
             } while (window * 2 > sequence);
         }
-        setDrop();
+        setDrop();          // Get packets to be dropped
         try {
-            DatagramSocket senderSocket = new DatagramSocket(9877);
-            DatagramSocket acknowledgementSocket = new DatagramSocket(9878);
-            InetAddress IPAddress = InetAddress.getByName("localhost");
-            byte[] sendData = new byte[size];
-            byte[] ackData = new byte[size];
-            sendData = prepData(sendData);
-            DatagramPacket sendPkt = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-            DatagramPacket ackPkt = new DatagramPacket(ackData, sendData.length);
-            senderSocket.send(sendPkt);  //
+            DatagramSocket senderSocket = new DatagramSocket(9877);             // Set up sending socket
+            DatagramSocket acknowledgementSocket = new DatagramSocket(9878);    // Set up ack socket
+            InetAddress IPAddress = InetAddress.getByName("localhost");         // Get IP address
+            byte[] sendData = new byte[size];                                   // Set up array for data
+            byte[] ackData = new byte[size];                                    // Set up array for acks
+            sendData = prepData(sendData);                                      // Format data for sending
+            DatagramPacket sendPkt = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);    // Set up packet to send
+            DatagramPacket ackPkt = new DatagramPacket(ackData, sendData.length);   // Set up ack packet
+            senderSocket.send(sendPkt);                                         // Send packet
 
-            System.out.println("Send window's size and maximum seq. number to the reciever.");
-            int windowStart = 0;
-            char windowTracker[] = new char[sequence];
-            Timer windowTimer[] = new Timer[sequence];
-            for (int i = 0; i < sequence; i++) {
+            System.out.println("Send window's size and maximum seq. number to the reciever.");  // Message for communication
+            int windowStart = 0;                            // Set window base
+            char windowTracker[] = new char[sequence];      // Create array to track window
+            Timer windowTimer[] = new Timer[sequence];      // Create array of timers for packets
+            for (int i = 0; i < sequence; i++) {            // Set packets to not sent
                 windowTracker[i] = 'n';
             }
             /*  For windowTracker we will have the following 
@@ -97,23 +100,23 @@ public class Sender {
              *   a = Acknowledged
              *   n = not sent
              */
-            int i = 0;
-            boolean finished= false;
+            int i = 0;                      // Indexing variable
+            boolean finished= false;        // Looping variable
             do {
-                boolean sent = false;
-                if(i<sequence){
+                boolean sent = false;       // Variable for sent or not
+                if(i<sequence){             // As long as we arent at the end of the sequence
                 if (i < window) { // if i is sending the first packets of the window
-                    sendData[0] = (byte) i;
-                    sendPkt = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-                    senderSocket.send(sendPkt);
-                    sent = true;
-                } else if (windowTracker[i - window] == 'a') {
+                    sendData[0] = (byte) i; // Set first spot to sequence number
+                    sendPkt = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);   // Set up packet
+                    senderSocket.send(sendPkt);                                                 // Send packet
+                    sent = true;                                                                // Set sent to true
+                } else if (windowTracker[i - window] == 'a') {                                  // If previous packets acked send next packet
                     sendData[0] = (byte) i;
                     sendPkt = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
                     senderSocket.send(sendPkt);
                     sent = true;
                 }
-                if (sent == true) {
+                if (sent == true) {         // If sent print message, set timer and scheduel resend if needed then increment i
                     windowTracker[i] = 's';
                     System.out.println("Packet " + i + " is sent, window" + messageSender(windowStart, windowTracker));
                     windowTimer[i] = new Timer();
@@ -121,26 +124,27 @@ public class Sender {
                     i++;
                 }
                 }
-                if (sent == false) { // if we are at the edge of our window, we listen
+                if (sent == false) { // if we are at the edge of our window, we listen for acknowledgements
                     acknowledgementSocket.receive(ackPkt);
                     ackData = ackPkt.getData();
                     int ack = ackData[0];
                     
                     windowTracker[ack] = 'a';
-                    System.out.println("Ack "+ack+" received, window"+messageSender(windowStart,windowTracker));
-                    windowTimer[ack].cancel();
+                    System.out.println("Ack "+ack+" received, window"+messageSender(windowStart,windowTracker));    // Print message for acked packet
+                    windowTimer[ack].cancel();                                                                      // Cancel its timer
                 }
-                while(windowTracker[windowStart] == 'a' && windowStart != sequence-1){
+                while(windowTracker[windowStart] == 'a' && windowStart != sequence-1){                              // Loop through sequence
                     windowStart++;
                 }
                 finished = finish(windowTracker);
-            } while (finished!= true);
+            } while (finished!= true);                                                                              // Stop when all packets sent and acked
 
         } catch (IOException exc) {
-            Logger.getLogger(Sender.class.getName()).log(Level.SEVERE, null, exc);
+            Logger.getLogger(Sender.class.getName()).log(Level.SEVERE, null, exc);                                  // Catch IOExceptions
         }
     }
-
+    
+    // Make sure all packets have been sent sand aked
     public boolean finish(char[] array){
         for(int i =0; i< array.length;i++){
             if(array[i] != 'a'){
